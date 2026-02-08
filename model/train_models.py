e44f11b6b1a993513b1d1bce5bbc88c48c489564
Azhar <azharnda@gmail.com>
2026-02-08 13:56:59 +0530

    separate training modules for each ML model & train all_model together

---
+++ model/train_models.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from preprocessing import load_and_preprocess_data
+from evaluation import evaluate_model
+
+from logistic_model import train_logistic_regression
+from decision_tree_model import train_decision_tree
+from naive_bayes_model import train_naive_bayes
+from random_forest_model import train_random_forest
+from knn_model import train_knn
+from xgboost_model import train_xgboost
+
+def train_all_models(csv_path):
+    X_train, X_test, y_train, y_test = load_and_preprocess_data(csv_path)
+
+    models = {
+        'Logistic Regression': train_logistic_regression(X_train, y_train),
+        'Decision Tree': train_decision_tree(X_train, y_train),
+        'Naive Bayes': train_naive_bayes(X_train, y_train),
+        'Random Forest': train_random_forest(X_train, y_train),
+        'KNN': train_knn(X_train, y_train),
+        'XGBoost': train_xgboost(X_train, y_train)
+    }
+
+    results = []
+    for name, model in models.items():
+        metrics = evaluate_model(model, X_test, y_test)
+        results.append({
+            "Model": name,
+            "Accuracy": metrics["Accuracy"],
+            "AUC": metrics["AUC"],
+            "Precision": metrics["Precision"],
+            "Recall": metrics["Recall"],
+            "F1": metrics["F1"],
+            "MCC": metrics["MCC"]
+        })
+    return pd.DataFrame(results), models
---
+++ model/xgboost_model.py
@@ -0,0 +1,10 @@
+from xgboost import XGBClassifier
+
+def train_xgboost(X_train, y_train):
+    model = XGBClassifier(
+        eval_metric="logloss",
+        use_label_encoder=False,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
+    return model
